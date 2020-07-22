b912d7ffff084fe3338ca28863d76f076c31f1cb
SatoshiLTCP <SatoshiLTCP>
2020-07-22 14:16:32 +0200

    params change

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -217,8 +217,7 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
         // nodes with support for servicebits filtering should be at the top
-        /*vSeeds.emplace_back("testnet-seed1.litecoinpos.net");
-        vSeeds.emplace_back("testnet-seed2.litecoinpos.net");*/
+        vSeeds.emplace_back("testnet-seed1.tokl.io");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,65);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,78);
